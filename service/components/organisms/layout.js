0591b1533395a2fd064112e7dfa1d507c50b305d
Lodi <david@Davids-Mac-mini.local>
2020-11-13 15:09:50 -0600

    🚨 Para que el niño acomode 🚨

--- service/components/organisms/layout.js
+++ service/components/organisms/layout.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
+import Cursor from "../atoms/cursor";
 import Navbar from "../molecules/navbar";
 import AreaLanguages from "../molecules/area-languages";
 import Footer from "../molecules/footer";
 
-export default function Layout({ children, toggle }) {
+export default function Layout({ children }) {
+    
   return (
     <>
+      <Cursor />
       <Navbar />
       <AreaLanguages />
       {children}
