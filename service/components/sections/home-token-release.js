a7de5365aee00b455d268c00f7a4b0e42737baed
Lodi <david@Davids-Mac-mini.local>
2020-11-17 14:47:49 -0600

    📝  type table

--- service/components/sections/home-token-release.js
+++ service/components/sections/home-token-release.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function HomeTokenRelease() {
   return (
@@ -8,25 +8,46 @@ export default function HomeTokenRelease() {
           <div>
             <h2>TOKEN RELEASE</h2>
             <p>
-              Lock HOPR tokens in your node to relay user data and cover traffic. The more you stake, the more data you
-              can relay, and the more you earn. A quarter of all HOPR tokens are available only to stakers as a reward
-              for relaying cover traffic.
+              Lock HOPR tokens in your node to relay user data and cover
+              traffic. The more you stake, the more data you can relay, and the
+              more you earn. A quarter of all HOPR tokens are available only to
+              stakers as a reward for relaying cover traffic.
             </p>
           </div>
-
+          <div className="table-info">
+            <div className="container-table">
+              <div className="title">
+                <h4>KEY METRICS</h4>
+              </div>
+              <div className="list-table">
+                <ul>
+                  <li>Token Name: HOPR Token</li>
+                  <li>Token Symbol: HOPR</li>
+                  <li>Network: Ethereum</li>
+                  <li>Standards: ERC20, ERC777</li>
+                  <li>Total supply: X HOPR</li>
+                  <li>
+                    Initial circulating supply: X HOPR• Token price: X USD /
+                    HOPR
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
           <div>
-            <h2>Token Allocation (%)</h2>
+            <h3>Token Allocation (%)</h3>
             <p>
-              Lock HOPR tokens in your node to relay user data and cover traffic. The more you stake, the more data you
-              can relay, and the more you earn. A quarter of all HOPR tokens are available only to stakers as a reward
-              for relaying cover traffic.
+              Lock HOPR tokens in your node to relay user data and cover
+              traffic. The more you stake, the more data you can relay, and the
+              more you earn. A quarter of all HOPR tokens are available only to
+              stakers as a reward for relaying cover traffic.
             </p>
           </div>
           <div>
-            <h2>Token Supply Distribution (%)</h2>
+            <h3>Token Supply Distribution (%)</h3>
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
