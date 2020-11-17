f1a2d0304d282d07fdae08971e3131ca699063c0
Lodi <david@Davids-Mac-mini.local>
2020-11-17 12:44:15 -0600

    🎨 footer

--- service/components/molecules/footer.js
+++ service/components/molecules/footer.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import Link from "next/link";
 
 const dataInfo = [
   {
@@ -34,23 +35,58 @@ const dataInfo = [
 export default function Footer() {
   return (
     <footer>
-      <div>
-        <div className="footer-icon">
-          <img src="/assets/icons/horp_icon.svg" alt="The HOPR-Token footer" />
+      <div className="aux-container">
+        <div className="first-line-footer">
+          <div className="footer-icon">
+            <img
+              src="/assets/icons/horp_icon.svg"
+              alt="The HOPR-Token footer"
+            />
+          </div>
+          <div className="footer-social">
+            <ul className="list-reset">
+              {dataInfo.map((e, index) => {
+                const { img, link } = e;
+                return (
+                  <li key={index}>
+                    <a href={link} target="_blank" rel="noopener noreferrer">
+                      <img src={img} alt="The HOPR-Token NOW" />
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
-        <div className="footer-social">
-          <ul className="list-reset">
-            {dataInfo.map((e, index) => {
-              const { img, link } = e;
-              return (
-                <li key={index}>
-                  <a href={link} target="_blank" rel="noopener noreferrer">
-                    <img src={img} alt="The HOPR-Token NOW" />
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
+        <div className="second-line-footer">
+          <div>
+            <p className="copy-footer">
+              &copy;{new Date().getFullYear()} HORP Association, all rights
+              reserved
+            </p>
+          </div>
+          <div className="footer-menu">
+            <ul>
+              <li>
+                <Link href="/about-us">Contact</Link>
+              </li>
+              <li>
+                <Link href="/about-us">About us</Link>
+              </li>
+              <li>
+                <Link href="/about-us">Partners</Link>
+              </li>
+              <li>
+                <Link href="/about-us">Support</Link>
+              </li>
+              <li>
+                <Link href="/about-us">FAQ</Link>
+              </li>
+              <li>
+                <Link href="/about-us">Disclaimer</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </footer>
