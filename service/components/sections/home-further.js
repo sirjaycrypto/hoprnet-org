894a8def60f2241e9415c03f78062785d52570f6
Lodi <david@Davids-Mac-mini.local>
2020-11-18 20:14:22 -0600

    🚀  add further section

--- service/components/sections/home-further.js
+++ service/components/sections/home-further.js
@@ -1,24 +1,80 @@
 import React, { useEffect, useState } from 'react'
 
 export default function HomeFurther() {
+  const infoTop = [
+    {
+      img: "/assets/brand/logo.svg",
+      title: "Install HOPR-Node for free via GitHub",
+      paragraph:
+        "Anyone can run a HOPR node on their own hardware! We provide easy-to-install software for Linux, macOS, Windows, Docker and Cloud Environments.",
+    },
+    {
+      img: "/assets/brand/logo.svg",
+      title: "Buy HOPR-Node",
+      paragraph: `We need a copy. Anyone can run a HOPR node on their own hardware! We provide easy-to-install software for Linux, macOS, Windows, Docker and Cloud Environments.`,
+    },
+  ];
+
+  const infoDown = [
+    {
+      img: "/assets/brand/logo.svg",
+      title: "Our Blog",
+    },
+    {
+      img: "/assets/brand/logo.svg",
+      title: "Our Values",
+    },
+    {
+      img: "/assets/brand/logo.svg",
+      title: "Our Whitepaper",
+    },
+    {
+      img: "/assets/brand/logo.svg",
+      title: "Our organizational structure: Decent Gov",
+    },
+    {
+      img: "/assets/brand/logo.svg",
+      title: "HOPR in the Media",
+    },
+
+  ];
+
   return (
     <>
-      <section className="section-further">
+      <section className="section-further padding-section-aux">
         <div className="aux-container">
           <h2>FURTHER READING</h2>
-          <div className="item-back-container">
-            <div className="item-logo-element">
-              <p>logo</p>
-            </div>
-            <div className="item-logo-element">
-              <p>logo</p>
-            </div>
-            <div className="item-logo-element">
-              <p>logo</p>
-            </div>
-            <div className="item-logo-element">
-              <p>logo</p>
-            </div>
+          <div className="list-top">
+          {infoTop.map((e, index) => {
+                const { img, title, paragraph } = e;
+                return (
+                  <div key={index} className="element-future">
+                    <div className="icon">
+                      <img src={img} alt={title} />
+                    </div>
+                    <div className="info">
+                      <h5>{title}</h5>
+                      <p>{paragraph}</p>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+
+          <div className="list-down">
+          {infoDown.map((e, index) => {
+                const { img, title } = e;
+                return (
+                  <div key={index} className="element-future">
+                    <div className="icon">
+                      <img src={img} alt={title} />
+                    </div>
+                    <div className="info">
+                      <h5>{title}</h5>
+                    </div>
+                  </div>
+                );
+              })}
           </div>
         </div>
       </section>
--- service/pages/index.js
+++ service/pages/index.js
@@ -7,6 +7,8 @@ import HomeTokenFeatures from "../components/sections/home-token-features";
 import HomeHardware from "../components/sections/home-hardware";
 import HomeTokenReleas from "../components/sections/home-token-release";
 import HomeBacked from "../components/sections/home-backed";
+import HomeFurther from "../components/sections/home-further";
+
 import Slide from "../components/organisms/slider";
 export default function Home() {
   return (
@@ -32,6 +34,7 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <HomeFurther />
     </Layout>
   );
 }
