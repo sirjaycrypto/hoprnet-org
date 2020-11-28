136e72115c9870833b1a2066e7a005474b0c5e34
lodi <lodi.dev@outlook.com>
2020-11-28 08:20:35 -0600

    ✨  update for deploy

--- service/components/molecules/header.js
+++ service/components/molecules/header.js
@@ -1,26 +1,23 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 export default function Header() {
   return (
     <Head>
       <title>HOPR</title>
-    
+
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="canonical" href="robot-data.io" />
+      <link rel="canonical" href="https://hoprnet.org/" />
       <meta name="theme-color" content="#ffffa0" />
       <meta
         name="description"
-        content=""
+        content="The HOPR protocol ensures everyone has control of their privacy, data, and identity."
       />
       {/* <meta property="og:image" content="/assets/brand/meta.jpg" />
       <meta property="og:image:alt" ccontent="assets/brand/meta.jpg" /> */}
       <meta charset="utf-8" />
-      <meta
-        name="keywords"
-        content=""
-      />
-      <meta name="author" content="" />
-      <meta name="copyright" content="" />
+      <meta name="keywords" content="crypto, data privacy, network-level" />
+      <meta name="author" content="HOPR" />
+      <meta name="copyright" content=" HOPR" />
       <meta name="robots" content="index,nofollow" />
       <meta http-equiv="cache-control" content="no-cache" />
       <meta http-equiv="expires" content="43200" />
