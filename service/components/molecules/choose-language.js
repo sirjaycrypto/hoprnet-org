2bed2a83efbd675b7e8526721791dd2e7421f3ec
lodi <lodi.dev@outlook.com>
2020-11-30 17:33:19 -0600

    🎨

--- service/components/molecules/choose-language.js
+++ service/components/molecules/choose-language.js
@@ -1,58 +1,65 @@
-import React from 'react';
-import Link from 'next/link';
-import useTranslation from 'next-translate/useTranslation';
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { css } from "@emotion/css";
+import useTranslation from "next-translate/useTranslation";
+
 const listLang = [
   {
-    info: 'de',
-    label: 'German',
+    info: "de",
+    label: "German",
   },
   {
-    info: 'en',
-    label: 'English',
+    info: "en",
+    label: "English",
   },
   {
-    info: 'ma',
-    label: 'Mandarin',
+    info: "ma",
+    label: "Mandarin",
   },
   {
-    info: 'pt',
-    label: 'Portuguese',
+    info: "pt",
+    label: "Portuguese",
   },
   {
-    info: 'rus',
-    label: 'Russian',
+    info: "rus",
+    label: "Russian",
   },
   {
-    info: 'ko',
-    label: 'Korean',
+    info: "ko",
+    label: "Korean",
   },
   {
-    info: 'es',
-    label: 'Spanish',
+    info: "es",
+    label: "Spanish",
   },
   {
-    info: 'ja',
-    label: 'Japanese',
+    info: "ja",
+    label: "Japanese",
   },
 ];
 export default function ChooseLanguage() {
+  const [translateY, settranslateY] = useState(102);
   const { t, lang } = useTranslation();
   return (
     <div className="container-languages">
       <div>
-        <div>{lang}</div>
-        <ul>
-          {listLang.map((e, i) => {
-            const { info, label } = e;
-            return (
-              lang !== info && (
-                <Link href="/" locale={info} key={i}>
-                  <li>{label}</li>
-                </Link>
-              )
-            );
-          })}
-        </ul>
+        <div className="select-lang">{lang}</div>
+        <div className="box-hidden-info">
+          <ul   className={css`
+           transform: translateY("${translateY}"%);
+          `}>
+            {listLang.map((e, i) => {
+              const { info, label } = e;
+              return (
+                lang !== info && (
+                  <Link href="/" locale={info} key={i}>
+                    <li>{label}</li>
+                  </Link>
+                )
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
