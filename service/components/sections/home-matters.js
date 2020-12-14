d0ecfcec14c9780fd365da5e93e83189bf26d5b7
Alfredo Medrano Sánchez <alfredomedranosanchez@gmail.com>
2020-12-14 15:41:01 -0600

    Update chart

--- service/components/sections/home-matters.js
+++ service/components/sections/home-matters.js
@@ -1,48 +1,28 @@
-import React, { forwardRef, useEffect } from "react";
-import useTranslation from "next-translate/useTranslation";
-import { Line } from "react-chartjs-2";
-
+import React, { forwardRef, useState, createRef, useEffect } from 'react';
+import useTranslation from 'next-translate/useTranslation';
+import { Line } from 'react-chartjs-2';
+const PARTS = 100;
+const PERCENT_BRANCH = 0.68;
 const HomeMatter = forwardRef(({ setVisibleNow }, ref) => {
   const { t } = useTranslation();
+  const [chartSizes, setChartSizes] = useState({ width: 0, height: 0 });
+  const chartRef = createRef();
   const data = {
     labels: [
-      "2009",
-      "2010",
-      "2011",
-      "2012",
-      "2013",
-      "2014",
-      "2015",
-      "2016",
-      "2017",
-      "2018",
-      "2019",
-      "2020",
-      "2021",
-      "2022",
-    ],
-    datasets: [
-      {
-        data: [
-          241812,
-          368808,
-          522840,
-          614016,
-          718176,
-          869112,
-          1152648,
-          1464000,
-          1810908,
-          2237436,
-          2740932,
-          3337284,
-          4200000,
-        ],
-        borderDash: [1, 0],
-        fill: false,
-        backgroundColor: "rgba(0,0,0,0)",
-        borderColor: "#648CFA",
-      },
+      '2009',
+      '2010',
+      '2011',
+      '2012',
+      '2013',
+      '2014',
+      '2015',
+      '2016',
+      '2017',
+      '2018',
+      '2019',
+      '2020',
+      '2021',
+      '2022',
     ],
   };
 
@@ -53,7 +33,10 @@ const HomeMatter = forwardRef(({ setVisibleNow }, ref) => {
       spanGaps: false,
     },
     responsive: true,
-    borderCapStyle: "round",
+    borderCapStyle: 'round',
+    onResize: (props) => {
+      setChartSizes(props);
+    },
     elements: {
       point: {
         radius: 0,
@@ -71,7 +54,7 @@ const HomeMatter = forwardRef(({ setVisibleNow }, ref) => {
             min: 0.0,
             stepSize: 1000000,
             callback: function (value, index, values) {
-              return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+              return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
             },
           },
         },
@@ -90,7 +73,51 @@ const HomeMatter = forwardRef(({ setVisibleNow }, ref) => {
       ],
     },
   };
+  const _handleParseData = () => {
+    let { width } = chartSizes || chartRef.current?.chartInstance,
+      preWidth = 3,
+      aResult = [
+        {
+          data: [
+            241812,
+            368808,
+            522840,
+            614016,
+            718176,
+            869112,
+            1152648,
+            1464000,
+            1810908,
+            2237436,
+            2740932,
+            3337284,
+            4200000,
+          ],
+          borderDash: [],
+          fill: false,
+          backgroundColor: 'rgba(0,0,0,0)',
+          borderColor: '#648CFA',
+        },
+      ];
+    aResult[0].borderDash.push(width * PERCENT_BRANCH);
+    aResult[0].borderDash.push(4);
+    for (let i = 0; i <= PARTS; i++) {
+      aResult[0].borderDash.push(preWidth);
+    }
+    return aResult;
+  };
 
+  function _handleResize() {
+    if (chartRef.current !== null) {
+      setChartSizes(chartRef.current.chartInstance);
+    }
+  }
+  useEffect(() => {
+    _handleResize();
+    window.addEventListener('resize', _handleResize);
+
+    return () => window.removeEventListener('resize', _handleResize);
+  }, [_handleResize]);
   return (
     <section
       ref={ref}
@@ -99,18 +126,22 @@ const HomeMatter = forwardRef(({ setVisibleNow }, ref) => {
     >
       <div className="container">
         <div className="container-center-center">
-          <h2>{t("homeWhyMatt:title")}</h2>
+          <h2>{t('homeWhyMatt:title')}</h2>
         </div>
         <div className="flex-line-row">
           <div className="aux-padding-box-read">
-            <p>"{t("homeWhyMatt:mainText")}"</p>
+            <p>"{t('homeWhyMatt:mainText')}"</p>
           </div>
           <div>
             <div className="container-char">
-              <Line data={data} options={optionsLine} />
+              <Line
+                data={{ ...data, datasets: _handleParseData() }}
+                options={optionsLine}
+                ref={chartRef}
+              />
             </div>
             <div className="center-help">
-              <p className="link-out">{t("homeWhyMatt:labelDate")}</p>
+              <p className="link-out">{t('homeWhyMatt:labelDate')}</p>
             </div>
           </div>
         </div>
