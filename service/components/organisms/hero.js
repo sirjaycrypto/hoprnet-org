7482b12d33659813a84ee523894ac3ca711a89aa
lodi <lodi.dev@outlook.com>
2020-12-15 13:29:20 -0600

    🐛  fix hero video :D
    Former-commit-id: 46457f3a039484d9a21efca12d141d1f5a81e773

--- service/components/organisms/hero.js
+++ service/components/organisms/hero.js
@@ -3,30 +3,44 @@ import useTranslation from "next-translate/useTranslation";
 import HeroInfo from "../molecules/hero-info";
 
 const Hero = forwardRef(({ setVisibleNow }, ref) => {
-  const [videoActive, setVideoActive] = useState("");
+  const [videoL, setVideoL] = useState(false);
+  const [videoM, setVideoM] = useState(false);
+  const [videoS, setVideoS] = useState(false);
   const { t } = useTranslation();
 
+
+  
+  
   useEffect(() => {
     if (typeof window !== "undefined") {
       if (window.matchMedia("(min-width: 1080px)").matches) {
-        setVideoActive("/assets/video/hero.mp4");
-      } else if (window.matchMedia("(max-width: 797px)").matches) {
-        setVideoActive("/assets/video/hopr_Drone_Tablet-Portrait.mp4");
-      } else if (window.matchMedia("(max-width: 370px)").matches) {
-        setVideoActive("/assets/video/hopr_Drone_Mobile.mp4");
+        setVideoL(true);
+      } else if (window.matchMedia("(min-width: 768px)").matches) {
+        setVideoM(true);
+      } else if (window.matchMedia("(min-width: 370px)").matches) {
+        setVideoS(true);
       }
     }
-  });
+  },[]);
 
   return (
     <>
       <section ref={ref} className="section-hero">
         <HeroInfo />
         <video id="background-video" autoPlay loop muted>
-          <source
-            src="/assets/video/hopr_Drone_Tablet-Portrait.mp4"
-            type="video/mp4"
-          />
+          {videoL && <source src="/assets/video/hero.mp4" type="video/mp4" />}
+          {videoM && (
+            <source
+              src="/assets/video/hopr_Drone_Tablet-Portrait.mp4"
+              type="video/mp4"
+            />
+          )}
+          {videoS && (
+            <source
+              src="/assets/video/hopr_Drone_Mobile.mp4"
+              type="video/mp4"
+            />
+          )}
           Your browser does not support the video tag.
         </video>
 
