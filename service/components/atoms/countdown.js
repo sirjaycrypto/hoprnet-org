9ba4ace4dd697a4aa253b307766dfff6b1ea9c8f
lodi <lodi.dev@outlook.com>
2020-12-14 18:29:16 -0600

    🚀 more video
    Former-commit-id: c1d6ad0c431ef917f5caa984f70485d0a6ac2f3a

--- service/components/atoms/countdown.js
+++ service/components/atoms/countdown.js
@@ -1,25 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Countdown() {
-  const calculateTimeLeft = () => {
-    let year = new Date().getFullYear();
-    let difference = +new Date(`10/01/${year}`) - +new Date();
-    let timeLeft = {};
+  const [num, setNum] = useState(34);
 
-    if (difference > 0) {
-      timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
-      };
-    }
-
-    return timeLeft;
-  };
+  
   return (
     <div className="info-hero-top">
-      <p className="help-label-info">COUNTDOWN DD:HH:SS</p>
+      <p className="help-label-info">COUNTDOWN {num}</p>
     </div>
   );
 }
--- service/components/organisms/hero.js
+++ service/components/organisms/hero.js
@@ -1,16 +1,30 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useState, useEffect } from "react";
 import useTranslation from "next-translate/useTranslation";
 import HeroInfo from "../molecules/hero-info";
 
 const Hero = forwardRef(({ setVisibleNow }, ref) => {
+  const [videoActive, setVideoActive] = useState("");
   const { t } = useTranslation();
 
+  useEffect(() => {
+    if (window.matchMedia("screen and (min-width: 1080px)").matches) {
+      setVideoActive("/assets/video/hopr_Drone_Desktop_Tablet-Landscape.mp4");
+    }
+    if (window.matchMedia("screen and (max-width: 797px)").matches) {
+      setVideoActive("/assets/video/hopr_Drone_Tablet-Portrait.mp4");
+    }
+    if (window.matchMedia("screen and (max-width: 370px)").matches) {
+      setVideoActive("/assets/video/hopr_Drone_Mobile.mp4");
+      
+    }
+  });
+console.log(videoActive)
   return (
     <>
       <section ref={ref} className="section-hero">
         <HeroInfo />
         <video id="background-video" autoPlay loop muted>
-          <source src="/assets/video/hero.mp4" type="video/mp4" />
+          <source src={videoActive} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
