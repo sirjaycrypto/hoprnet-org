64c46785cfca5da17e6b6164eb744e016dd2661b
lodi <lodi.dev@outlook.com>
2020-12-17 13:15:09 -0600

    fix video
    Former-commit-id: 0b24abeff881d04d6934fad53cbb06e8d5a6fcfc

--- service/components/organisms/hero.js
+++ service/components/organisms/hero.js
@@ -3,73 +3,61 @@ import useTranslation from "next-translate/useTranslation";
 import HeroInfo from "../molecules/hero-info";
 
 const Hero = forwardRef(({}, ref) => {
-  const [videoL, setVideoL] = useState(false);
-  const [videoM, setVideoM] = useState(false);
-  const [videoS, setVideoS] = useState(false);
-  const [videoExp, setVideovideoExp] = useState(false);
+  // const [videoL, setVideoL] = useState(false);
+  // const [videoM, setVideoM] = useState(false);
+  // const [videoS, setVideoS] = useState(false);
+  // const [videoExp, setVideovideoExp] = useState(false);
   const { t } = useTranslation();
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      if (window.matchMedia("(min-width: 1080px)").matches) {
-        setVideoL(true);
-        console.log("L");
-      } else if (window.matchMedia("(max-width: 768px)").matches) {
-        setVideoM(true);
-        setVideovideoExp(true);
-        console.log("Mº");
-      } else if (window.matchMedia("(max-width: 639px)").matches) {
-        setVideoS(true);
-        console.log("S");
-      }
+    // if (typeof window !== "undefined") {
+    //   if (window.matchMedia("(min-width: 1080px)").matches) {
+    //     setVideoL(true);
+    //     console.log("L");
+    //   } else if (window.matchMedia("(max-width: 768px)").matches) {
+    //     setVideoM(true);
+    //     setVideovideoExp(true);
+    //     console.log("Mº");
+    //   } else if (window.matchMedia("(max-width: 639px)").matches) {
+    //     setVideoS(true);
+    //     console.log("S");
+    //   }
    
-    }
+    // }
   }, []);
 
   return (
     <>
       <section ref={ref} className="section-hero">
         <HeroInfo />
-        {videoL && (
+    
           <video
             autoPlay
             loop
             muted
             playsInline
-            className="background-video"
+            className="background-video display-desktop"
             src="/assets/video/hero.mp4"
           ></video>
-        )}
-        {videoM && (
+      
           <video
             autoPlay
             loop
             muted
             playsInline
-            className="background-video"
+            className="background-video display-tablet"
             src="/assets/video/hopr_Drone_Tablet-Portrait.mp4"
           ></video>
-        )}
-        {videoS && (
+      
           <video
             autoPlay
             loop
             muted
             playsInline
-            className="background-video"
+            className="background-video display-phone"
             src="/assets/video/hopr_Drone_Mobile.mp4"
           ></video>
-        )}
-    {videoExp && (
-          <video
-            autoPlay
-            loop
-            muted
-            
-            className="background-video"
-            src="/assets/video/hopr_Drone_Tablet-Portrait.mp4"
-          ></video>
-        )}
+
         <div className="container">
           <div className="text-wrapper">
             <h4>{t("homeHero:subtitle")}</h4>
