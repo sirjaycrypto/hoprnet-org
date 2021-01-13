1520e3343c5016cb8f701ceda4ecb9cc229e10ec
lodi <lodi.dev@outlook.com>
2021-01-13 15:40:07 -0600

    TEST
    Former-commit-id: dc5a86328f9befa267dc7007e31c01be0c76292b

--- service/components/organisms/hero.js
+++ service/components/organisms/hero.js
@@ -1,16 +1,16 @@
-import React, { forwardRef, useState, useEffect } from "react";
-import useTranslation from "next-translate/useTranslation";
-import Modal from "../atoms/modal";
-import HeroInfo from "../molecules/hero-info";
+import React, { forwardRef, useState, useEffect } from 'react';
+import useTranslation from 'next-translate/useTranslation';
+import Modal from '../atoms/modal';
+import HeroInfo from '../molecules/hero-info';
 
 const Hero = forwardRef(({}, ref) => {
   const [dateFinal, setDateFinal] = useState(false);
   const [modePreSales, setModePreSales] = useState(false);
   const [showModal, setShowModal] = useState(false);
-  const [days, setDays] = useState("");
-  const [hours, setHours] = useState("");
-  const [minutes, setMinutes] = useState("");
-  const [seconds, setSeconds] = useState("");
+  const [days, setDays] = useState('');
+  const [hours, setHours] = useState('');
+  const [minutes, setMinutes] = useState('');
+  const [seconds, setSeconds] = useState('');
   const { t } = useTranslation();
 
   useEffect(() => {}, []);
@@ -24,12 +24,12 @@ const Hero = forwardRef(({}, ref) => {
   };
 
   const clickBtn = () => {
-    console.log("Click");
+    console.log('Click');
   };
 
-  const countDownDate = new Date("Jan 31, 2021 0:00:00").getTime();
+  const countDownDate = new Date('Jan 31, 2021 0:00:00').getTime();
   function addCeroIfNeed(num) {
-    let result = ("0" + num).slice(-2);
+    let result = ('0' + num).slice(-2);
     return result;
   }
 
@@ -52,7 +52,7 @@ const Hero = forwardRef(({}, ref) => {
       setDateFinal(true);
     }
   }, 1000);
-  
+
   return (
     <>
       <section ref={ref} className="section-hero">
@@ -79,7 +79,7 @@ const Hero = forwardRef(({}, ref) => {
               />
             ) : (
               <>
-                <h4>{t("home:hero.subtitle")}</h4>
+                <h4>{t('home:hero.subtitle')}</h4>
 
                 <div id="clockDiv">
                   <div>
@@ -101,7 +101,7 @@ const Hero = forwardRef(({}, ref) => {
 
                 <div>
                   <div onClick={() => clickBtn()} className="btn-banner ">
-                    <span>{t("common:getHorpFull")}</span>
+                    <span>{t('common:getHorpFull')}</span>
                   </div>
                 </div>
               </>
@@ -114,14 +114,14 @@ const Hero = forwardRef(({}, ref) => {
             <div>
               <p>Subscribe to be notified when the token is on sale </p>
               <div onClick={() => showModalActive()} className="btn-small ">
-                <span>{t("common:subscribe")}</span>
+                <span>{t('common:subscribe')}</span>
               </div>
             </div>
 
             <div>
               <p>Join our Telegram-Group</p>
               <div className="btn-small ">
-                <span>{t("common:join")}</span>
+                <span>{t('common:join')}</span>
               </div>
             </div>
           </div>
