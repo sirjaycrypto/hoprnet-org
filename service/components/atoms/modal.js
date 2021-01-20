364c9af0072f187a0a11e3ae680cb0cb5101e7a5
lodi <lodi.dev@outlook.com>
2021-01-20 14:52:34 -0600

    modaling work's :D
    Former-commit-id: 0179ada7a10fda9149de62a9a014aa8d3bed16b4

--- service/components/atoms/modal.js
+++ service/components/atoms/modal.js
@@ -13,22 +13,17 @@ const Modal = forwardRef(({ showModal, showModalActive }, ref) => {
         </div>
         <div className="main-modal">
           <div className="area-label-modal">
-            <h2>Disclaimer</h2>
-            <p>
-              By clicking on this link, you will be redirected to a website
-              provided by a third-party (third-party website). HOPR (likewise
-              any affiliated entity) has no control over the content, offerings
-              (including offerings regarding HOPR tokens) or resources of the
-              third-party website, and accepts no responsibility for the
-              third-party website, the content, offerings or resources, or for
-              any loss or damage that may arise from your use of the third
-              party-website, the content, offerings or resources. If you decide
-              to access the third-party website, you do so entirely at your own
-              risk and subject to the terms and conditions of use for the
-              third-party website.
-            </p>
+            <h2>{t('home:modal.title')}</h2>
+            <p>{t('home:modal.about')}</p>
           </div>
-          <div className="area-btn-modal"></div>
+          <div className="area-btn-modal">
+            <div className="btn-alt-modal ">
+              <span>{t('common:btn-Cancel')}</span>
+            </div>
+            <div className="btn-modal ">
+              <span>{t('common:btn-IAgree')}</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
