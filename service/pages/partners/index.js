2894e3fbc3dc067fd1b3bf9099d0f6e4f19c1579
Beto Salas <betinho8901@gmail.com>
2021-02-04 17:30:16 -0600

    Added new functionality to improve the pages

--- service/pages/partners/index.js
+++ service/pages/partners/index.js
@@ -2,56 +2,10 @@ import React from 'react';
 import useTranslation from 'next-translate/useTranslation';
 import Trans from 'next-translate/Trans';
 
+import { SectionContainer } from '../../components';
 import Layout from '../../components/organisms/layout';
-import HeroInternal from '../../components/organisms/hero-internal';
-import SubStrack from '../../components/molecules/sub-strack';
-import Jobs from '../../components/molecules/jobs';
-import ProfileAssociation from '../../components/molecules/profile-association';
-import PeopleBuild from '../../components/molecules/PeopleBuild';
-import ContactPlus from '../../components/molecules/contact-plus';
-import HomeInvestors from '../../components/sections/home-investors';
-
 import { loadNamespaces } from '../_app';
 
-const dataInfo = [
-  'assets/partners/01_avado.svg',
-  'assets/partners/02_froriep.svg',
-  'assets/partners/03_elrond.svg',
-  'assets/partners/04_sedimentum.svg',
-  'assets/partners/05_swiss_medtech.svg',
-  'assets/partners/06_dai_logo.svg',
-  'assets/partners/07_health_tech_cluster.svg',
-  'assets/partners/08_swiss_healthcare_startups.svg',
-];
-
-const dataVALUES = [
-  {
-    img: 'assets/icons/with-yellow-ball/protection-shield.png',
-    title: 'about:values.itemTitleA',
-    text: 'about:values.itemAboutA',
-    delayTime: '100',
-  },
-  {
-    img: 'assets/icons/with-yellow-ball/light-bulb-shine.png',
-    title: 'about:values.itemTitleB',
-    text: 'about:values.itemAboutB',
-    delayTime: '150',
-  },
-  {
-    img: 'assets/icons/with-yellow-ball/multiple-users-1.png',
-    title: 'about:values.itemTitleC',
-    text: 'about:values.itemAboutC',
-    delayTime: '200',
-  },
-];
-
-const SectionContainer = ({ children }) => {
-  return (
-    <section className="continue-hero-internal padding-section-aux invert-color ">
-      <div className="container">{children}</div>
-    </section>
-  );
-};
 export default function Index() {
   const { t } = useTranslation();
 
@@ -64,15 +18,15 @@ export default function Index() {
   return (
     <div className="partners-page">
       <Layout>
-        <HeroInternal>
+        <SectionContainer align="center" extraClass="header">
           <h1 data-aos="fade-down" data-aos-delay="150">
             {t('partners:hero.title')}
           </h1>
-          <div>
+          <div data-aos="fade-down" data-aos-delay="300">
             <Trans
               i18nKey="partners:hero.content"
               components={{
-                paragraph: <p data-aos="fade-down" data-aos-delay="300" />,
+                paragraph: <p />,
                 breakLine: <br />,
               }}
             />
@@ -84,51 +38,47 @@ export default function Index() {
               }}
             />
           </div>
-        </HeroInternal>
-        <SectionContainer>
+        </SectionContainer>
+        <SectionContainer align="center" invert={true}>
           <div data-aos="fade-zoom-in" data-aos-easing="ease-in-back">
             <h2>{t('partners:video.title')}</h2>
           </div>
-          <div className="container-sm two-block">
-            <iframe
-              title="The HOPR Advantage"
-              width="800"
-              height="500"
-              src="https://www.youtube-nocookie.com/embed/vb7mD8dp11Q"
-              frameBorder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            />
+          <iframe
+            title="The HOPR Advantage"
+            width="800"
+            height="500"
+            src="https://www.youtube-nocookie.com/embed/vb7mD8dp11Q"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </SectionContainer>
+        <SectionContainer align="center" containerSize='sm'>
+          <div data-aos="fade-zoom-in" data-aos-easing="ease-in-back">
+            <h2> {t('partners:protocol.title')}</h2>
+          </div>
+          <div className="content-columns">
+            <div className="col col-9" data-aos="fade-down">
+              <Trans
+                i18nKey="partners:protocol.leftContent"
+                components={{
+                  paragraph: <p data-aos="fade-down" data-aos-delay="300" />,
+                  breakLine: <br />,
+                }}
+              />
+            </div>
+            <div className="col col-9" data-aos="fade-down">
+              <Trans
+                i18nKey="partners:protocol.rightContent"
+                components={{
+                  paragraph: <p data-aos="fade-down" data-aos-delay="300" />,
+                  breakLine: <br />,
+                }}
+              />
+            </div>
           </div>
         </SectionContainer>
-        <section className="section-partners padding-section-aux">
-          <div className="container">
-            <div data-aos="fade-zoom-in" data-aos-easing="ease-in-back">
-              <h2> {t('partners:protocol.title')}</h2>
-            </div>
-            <div className="container-sm two-block">
-              <div className="block-left" data-aos="fade-down">
-                <Trans
-                  i18nKey="partners:protocol.leftContent"
-                  components={{
-                    paragraph: <p data-aos="fade-down" data-aos-delay="300" />,
-                    breakLine: <br />,
-                  }}
-                />
-              </div>
-              <div className="block-right" data-aos="fade-down">
-                <Trans
-                  i18nKey="partners:protocol.rightContent"
-                  components={{
-                    paragraph: <p data-aos="fade-down" data-aos-delay="300" />,
-                    breakLine: <br />,
-                  }}
-                />
-              </div>
-            </div>
-          </div>
-        </section>
-        <SectionContainer>
+        <SectionContainer align="center" invert={true}>
           <div data-aos="fade-zoom-in" data-aos-easing="ease-in-back">
             <h2>{t('partners:partnerSection.title')}</h2>
           </div>
