d1f99c7a532de81798e4d3838856cc115426a84e
Beto Salas <betinho8901@gmail.com>
2021-02-04 23:37:41 -0600

    Add break line into the address and also reduce the font size of the countdown for phone device #18

--- service/components/atoms/location-marker.js
+++ service/components/atoms/location-marker.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function LocationMarker() {
   return (
     <div className='info-hero-bottom'>
-      <p className="help-label-info">Jungfraujoch, Switzerland, 3454 masl</p>
+      <p className="help-label-info">Jungfraujoch, Switzerland,<br /> 3454 masl</p>
     </div>
   );
 }
