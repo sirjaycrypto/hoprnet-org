ab964e867ffa19f223ec4c02260f4f623299b710
Ruddy Garcia <ruddygasol94@gmail.com>
2021-02-12 15:59:15 -0600

    Fix for components

--- service/components/index.js
+++ service/components/index.js
@@ -1,5 +1,6 @@
 export * from './atoms/accordion-item';
 export * from './atoms/alert-msg';
 export * from './atoms/button';
+export * from './atoms/countdown';
 export * from './molecules/accordion';
 export * from './molecules/section';
