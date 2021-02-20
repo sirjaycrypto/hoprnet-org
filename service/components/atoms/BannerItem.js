1c26bd9be2358dbf88d87d3f222bcd27badc88d6
Ruddy Garcia <ruddygasol94@gmail.com>
2021-02-19 22:28:59 -0600

    design: Fixes #127 - Adding video behavior

--- service/components/atoms/BannerItem.js
+++ service/components/atoms/BannerItem.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import Link from 'next/link';
 
 export const BannerItem = ({
   alt,
@@ -9,22 +8,37 @@ export const BannerItem = ({
   height,
   link,
   onClick,
+  playing,
   src,
   video,
   width
 }) => {
+  const [isDesktop, setDesktop] = useState(true);
+
+  const setDeviceType = () => {
+    if (window.innerWidth < 1024) {
+      setDesktop(false);
+    } else  {
+      setDesktop(true);
+    }
+  };
+
+  useEffect(() => {
+    setDeviceType();
+  }, []);
 
   const renderContent = () => (
     <div className="banner-element" onClick={onClick}>
       <span>{blindText}</span>
       {!children ?
         video ? (
-          <div className="embed-container">
+          <div className={`embed-container`}>
             <iframe
               allowFullScreen
+              allow="autoplay; fullscreen"
               className="video"
               frameBorder="0"
-              src={`${video}?title=0&byline=0&portrait=0&playsinline=0&controls=1&loop=0&app_id=122963&width=100%`}
+              src={`${video}?title=0&autoplay=${playing}&byline=0&portrait=0&playsinline=0&controls=${isDesktop ? 0 : 1}&loop=0&app_id=122963`}
             ></iframe>
           </div>
         ) : src ? (
@@ -51,7 +65,12 @@ export const BannerItem = ({
 BannerItem.propTypes = {
   alt: PropTypes.string,
   blindText: PropTypes.string,
+  children: PropTypes.node,
   height: PropTypes.string,
+  link: PropTypes.string,
+  onClick: PropTypes.func,
+  playing: PropTypes.number,
   src: PropTypes.string,
+  video: PropTypes.string,
   width: PropTypes.string,
 };
--- service/components/molecules/banner-grid.js
+++ service/components/molecules/banner-grid.js
@@ -1,65 +1,61 @@
-import React from 'react';
+import { set } from 'lodash';
+import React, { useState } from 'react';
 import { BannerItem, SectionContainer } from '..';
 
-const onClickBanner = () => {
-  console.log('Aqui');
-};
+const aVideos = [
+  {
+    title: 'Jungfrau Release',
+    video: 'https://player.vimeo.com/video/513101933',
+  },
+  {
+    title: 'Our Team',
+    video: 'https://player.vimeo.com/video/513096259',
+  },
+  {
+    title: 'HOPR Hardware Node',
+    video: 'https://player.vimeo.com/video/513096240',
+  },
+  {
+    title: 'Why Jungfraujoch?',
+    video: 'https://player.vimeo.com/video/513096330',
+  },
+  {
+    title: 'Making of',
+    video: 'https://player.vimeo.com/video/513096151',
+  },
+  {
+    title: 'Our Values',
+    video: 'https://player.vimeo.com/video/513096293',
+  },
+  {
+    title: 'Tech Deepdive',
+    video: 'https://player.vimeo.com/video/513105733',
+  },
+]
 
 export const BannerGrid = () => {
+  const [isPlaying, setPlay] = useState(-1);
+
+  const onClickBanner = (nIndex) => {
+    setPlay(isPlaying !== nIndex ? nIndex : -1);
+  };
+
   return (
     <SectionContainer extraClass="banners">
       <div className="banner-row">
+        {aVideos.map(({ title, video }, nIndex) => (
+          <BannerItem
+            blindText={title}
+            height="273.88px"
+            key={nIndex}
+            onClick={() => onClickBanner(nIndex)}
+            playing={nIndex === isPlaying ? 1 : 0}
+            video={video}
+            width="468px"
+          />
+        ))}
         <BannerItem
-          onClick={onClickBanner}
-          blindText="Jungfrau Release"
-          height="273.88px"
-          video="https://player.vimeo.com/video/513101933"
-          width="468px"
-        />
-        <BannerItem
-          onClick={onClickBanner}
-          blindText="Our Team"
-          height="273.88px"
-          video="https://player.vimeo.com/video/513096259"
-          width="468px"
-        />
-        <BannerItem
-          onClick={onClickBanner}
-          blindText="HOPR Hardware Node"
-          height="273.88px"
-          video="https://player.vimeo.com/video/513096240"
-          width="468px"
-        />
-        <BannerItem
-          onClick={onClickBanner}
-          blindText="Why Jungfraujoch?"
-          height="273.88px"
-          video="https://player.vimeo.com/video/513096330"
-          width="468px"
-        />
-        <BannerItem
-          onClick={onClickBanner}
-          blindText="Making of"
-          height="273.88px"
-          video="https://player.vimeo.com/video/513096151"
-          width="468px"
-        />
-        <BannerItem
-          onClick={onClickBanner}
-          blindText="Our Values"
-          height="273.88px"
-          video="https://player.vimeo.com/video/513096293"
-          width="468px"
-        />
-        <BannerItem
-          onClick={onClickBanner}
-          blindText="Tech Deepdive"
-          height="273.88px"
-          video="https://player.vimeo.com/video/513105733"
-          width="468px"
-        />
-        <BannerItem
-          alt="Banner 8"
+          alt="Download Screensaver"
           blindText="Download our Jungfraujoch-Screensaver"
           height="273.88px"
           link="https://assets.hoprnet.org/HOPR_Screensaver.mp4"
