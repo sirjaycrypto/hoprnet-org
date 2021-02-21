9b32f79c0d95ab6d7fbf49775b7eb6aa75f7b723
jjperezaguinaga <me@jjperezaguinaga.com>
2021-02-21 07:22:49 +0000

    bug: Fixes #141 - Removing phrase

--- service/pages/win/index.js
+++ service/pages/win/index.js
@@ -90,8 +90,7 @@ export const Index = () => {
               MySwitzerland. Our global launch day event will hop around the
               world to ten different destinations, from Tokyo to San Francisco.
               Each destination will also play host to local community events
-              such as AMAs with more HOPR token giveaways. Check out the full
-              schedule [here]. Finally, find the hidden eleventh destination to
+              such as AMAs with more HOPR token giveaways. Finally, find the hidden eleventh destination to
               win our grand prize: 50,000 HOPR tokens and a trip for two to
               Switzerland and the fabulous Jungfraujoch! The destinations will
               unlock throughout the day. See below the destination panel for
