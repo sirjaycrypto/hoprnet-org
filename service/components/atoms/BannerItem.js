251ca614116944acab5d712e52684f3d5e9e7868
Ruddy Garcia <ruddygasol94@gmail.com>
2021-02-22 10:22:24 -0600

    design: Fixes #146 - Showing video UI

--- service/components/atoms/BannerItem.js
+++ service/components/atoms/BannerItem.js
@@ -8,17 +8,17 @@ export const BannerItem = ({
   height,
   link,
   onClick,
-  playing,
   src,
   video,
-  width
+  width,
 }) => {
   const [isDesktop, setDesktop] = useState(true);
+  const [showingControls, setShow] = useState(false);
 
   const setDeviceType = () => {
     if (window.innerWidth < 1024) {
       setDesktop(false);
-    } else  {
+    } else {
       setDesktop(true);
     }
   };
@@ -28,36 +28,45 @@ export const BannerItem = ({
   }, []);
 
   const renderContent = (link) => {
-    const ImgWrapper = () => (<img
-      alt={alt}
-      height={height}
-      src={src}
-      width={width}
-    />)
+    const ImgWrapper = () => (
+      <img alt={alt} height={height} src={src} width={width} />
+    );
     return (
-    <div className="banner-element" onClick={onClick}>
-      <span>{blindText}</span>
-      {!children ?
-        video ? (
-          <div className={`embed-container`}>
-            <iframe
-              allowFullScreen
-              allow="autoplay; fullscreen"
-              className="video"
-              frameBorder="0"
-              src={`${video}?title=0&autoplay=${playing}&byline=0&portrait=0&playsinline=0&controls=${isDesktop ? 0 : 1}&loop=0&app_id=122963`}
-            ></iframe>
-          </div>
-        ) : src ? (
-          link ?
-          <a href={link} download target="_blank">
-            <ImgWrapper/>
-          </a> :
-          <ImgWrapper/>
-        ) : null
-        : children}
-    </div>
-  )};
+      <div
+        className="banner-element"
+        onClick={onClick}
+        onMouseEnter={() => setShow(true)}
+        onMouseLeave={() => setShow(false)}
+      >
+        <span>{blindText}</span>
+        {!children ? (
+          video ? (
+            <div className={`embed-container`}>
+              <iframe
+                allowFullScreen
+                allow="autoplay; fullscreen"
+                className="video"
+                frameBorder="0"
+                src={`${video}?title=0&autoplay=0&byline=0&portrait=0&playsinline=0&controls=${
+                  isDesktop ? (showingControls ? 1 : 0) : 1
+                }&loop=0&app_id=122963`}
+              ></iframe>
+            </div>
+          ) : src ? (
+            link ? (
+              <a href={link} download target="_blank">
+                <ImgWrapper />
+              </a>
+            ) : (
+              <ImgWrapper />
+            )
+          ) : null
+        ) : (
+          children
+        )}
+      </div>
+    );
+  };
 
   return link ? renderContent(link) : renderContent();
 };
