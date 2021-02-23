94df486d0400f973e8f3f19248d8618da965cf08
Ruddy Garcia <ruddygasol94@gmail.com>
2021-02-22 18:02:52 -0600

    design: Fixes #130 - Adding basic video function

---
+++ service/components/atoms/game-item.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Button, GameCountdown } from '..';
+
+export const GameItem = ({
+  date,
+  destination,
+  hour,
+  onClick,
+  to,
+  video = 'https://player.vimeo.com/video/508834454',
+  visible,
+}) => {
+  return (
+    <>
+    <div className="event-item" onClick={onClick}>
+      <p>{destination}</p>
+      <p>{hour}</p>
+      <GameCountdown endDate={date} />
+    </div>
+    {visible && (<div className="quiz">
+      <iframe
+        allowFullScreen
+        className="quiz-video"
+        frameBorder="0"
+        src={`${video}?autoplay=1&loop=1&title=0&byline=0&muted=true&responsive=true&controls=false&app_id=122963`}
+        width="100%"
+      ></iframe>
+      <div className="answer">
+        <input type="text" />
+        <Button
+          content="Submit"
+          to={to}
+        />
+      </div>
+    </div>)}
+    </>
+  );
+};
--- service/components/index.js
+++ service/components/index.js
@@ -4,6 +4,7 @@ export * from './atoms/BannerItem';
 export * from './atoms/button';
 export * from './atoms/countdown';
 export * from './atoms/game-countdown';
+export * from './atoms/game-item';
 export * from './molecules/accordion';
 export * from './molecules/banner-grid';
 export * from './molecules/section';
