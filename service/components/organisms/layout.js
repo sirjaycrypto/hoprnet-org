54236f497ea33fe36a3ab8aa2f2c43ed354dec11
Ruddy García <ruddygasol94@gmail.com>
2021-03-30 13:51:58 -0600

    design: Fixes #186 - Adding background mask

--- service/components/organisms/layout.js
+++ service/components/organisms/layout.js
@@ -19,6 +19,7 @@ export default function Layout({ visibleNow, children }) {
 
   return (
     <AnimatePresence>
+      <div className="init-mask" />
       <ScrollReveal
         ref={scrollReveal}
         children={() => (
