f40a8ac14767e2e011d497cd32b5473865b72f15
Ruddy García <ruddygasol94@gmail.com>
2021-06-15 15:23:06 -0500

    #250 - Enabling dev on not found item in trans

--- service/pages/_app.js
+++ service/pages/_app.js
@@ -11,13 +11,33 @@ import '../styles/main.scss';
 export async function loadNamespaces(namespaces, lang) {
   let res = {};
   for (let ns of namespaces) {
-    res[ns] = await import(`../locales/${lang}/${ns}.json`).then(
-      (m) => m.default
-    );
+    const oDev = await import(`../locales/dev/${ns}.json`)
+      .then(m => m.default);
+    let oCurrent = await import(`../locales/${lang}/${ns}.json`)
+      .then(m => m.default);
+    verifyEntries(oDev, oCurrent);
+
+    res[ns] = oCurrent;
   }
 
   return res;
 }
+
+const verifyEntries = (oDefault, oCurrent) => {
+  for (let sKey in oDefault) {
+    if (typeof oDefault[sKey] === 'string') {
+      if (!oCurrent[sKey]) {
+        oCurrent[sKey] = oDefault[sKey];
+      }
+    } else {
+      if (oCurrent[sKey]) {
+        verifyEntries(oDefault[sKey], oCurrent[sKey]);
+      } else {
+        oCurrent[sKey] = oDefault[sKey];
+      }
+    }
+  }
+};
 
 function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(true);
