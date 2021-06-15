b4fa2f5bbbb28a77f58ec6a512130c2b6a72a6d1
Ruddy García <ruddygasol94@gmail.com>
2021-06-15 18:28:13 -0500

    Epic #250 - Fix warning and replacing class with className

--- service/hooks/translation.js
+++ service/hooks/translation.js
@@ -1,5 +1,6 @@
-import parse from 'html-react-parser';
+import parse, { domToReact } from 'html-react-parser';
 import handler from 'next-translate/useTranslation';
+import React from 'react';
 
 /**
  * Translate HTML and text
@@ -7,15 +8,33 @@ import handler from 'next-translate/useTranslation';
  */
 export function useTranslation() {
   const { t } = handler();
+  const options = {
+    replace: domNode => {
+
+      if (!domNode.attribs) {
+        return;
+      }
+
+      if (domNode.attribs.class) {
+        domNode.attribs['className'] = domNode.attribs.class;
+        delete domNode.attribs.class;
+        
+        return domToReact(domNode);
+      }
+    },
+  };
+
   function isHtml(str) {
     return /<\/?[^>]*>/.test(str);
   }
+
   function translate(tag) {
     let element = t(tag);
     if (isHtml(element)) {
-      return parse(element);
+      return parse(element, options);
     }
     return element;
   }
+
   return { t: translate };
 }
