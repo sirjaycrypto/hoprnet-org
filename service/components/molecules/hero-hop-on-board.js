c099119f645a6f73f031f1a1e6f4972e6b248632
CristianGomezO <cristiangomezosorio17@gmail.com>
2022-02-09 11:08:00 -0500

    design #391 - buttons and jobs added

--- service/components/molecules/hero-hop-on-board.js
+++ service/components/molecules/hero-hop-on-board.js
@@ -1,9 +1,6 @@
 import React, { useEffect } from 'react';
 
-const HeroHoprOnBoard = ({
-  src,
-  text,
-}) => {
+const HeroHoprOnBoard = ({ src, text }) => {
   return (
     <>
       {/* section-board-top-video */}
@@ -22,7 +19,14 @@ const HeroHoprOnBoard = ({
           <div className="container">
             <div className="text-wrapper text-video-hop">
               <span className="main-title">{text}</span>
-              <span className="date-text-hopr">6th Jan. - 27th Apr.</span>
+              <span className="buttons-hero">6th Jan. - 27th Apr.</span>
+              <div className="bottom-buttons">
+                <span className="buttons-hero">Programme</span>
+                <span className="buttons-hero">Bounties</span>
+                <a href="#jobs-hop-on-board">
+                  <span className="buttons-hero">Jobs</span>
+                </a>
+              </div>
             </div>
           </div>
         ) : null}
