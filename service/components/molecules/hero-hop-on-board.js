3a25ec820e852bb1a2ffe5c1dda3d0d242ebb842
CristianGomezO <cristiangomezosorio17@gmail.com>
2022-02-11 13:12:40 -0500

    fixed buttons corrected

--- service/components/molecules/hero-hop-on-board.js
+++ service/components/molecules/hero-hop-on-board.js
@@ -1,6 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const HeroHoprOnBoard = ({ src, text }) => {
+  const [bottomButtons, setBottomButtons] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
+  const onScroll = () => {
+    const scrollY = window.pageYOffset;
+
+    if (scrollY > 0) {
+      setBottomButtons(true);
+    } else {
+      setBottomButtons(false);
+    }
+  };
+
   return (
     <>
       {/* section-board-top-video */}
@@ -20,9 +37,17 @@ const HeroHoprOnBoard = ({ src, text }) => {
             <div className="text-wrapper text-video-hop">
               <span className="main-title">{text}</span>
               <span className="buttons-hero">6th Jan. - 27th Apr.</span>
-              <div className="bottom-buttons">
-                <span className="buttons-hero">Programme</span>
-                <span className="buttons-hero">Bounties</span>
+              <div
+                className={`bottom-buttons ${
+                  bottomButtons ? 'bottom-buttons-fixed' : ''
+                } `}
+              >
+                <a href="#road-map-hop">
+                  <span className="buttons-hero">Programme</span>
+                </a>
+                <a href="https://gitcoin.co/hoprnet/active" target="_blank">
+                  <span className="buttons-hero">Bounties</span>
+                </a>
                 <a href="#jobs-hop-on-board">
                   <span className="buttons-hero">Jobs</span>
                 </a>
